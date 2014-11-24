6c0d007fd807bb2b785da516a9676c7bd1f812f1
Aram Kocharyan <akarmenia@gmail.com>
2014-11-24 16:16:11 +1100

    Added meteor package info.

--- package.js
+++ package.js
@@ -1,9 +1,13 @@
 Package.describe({
-  summary: "A jQuery widget for specifying event recurrences in scheduling systems."
+  name: 'aramk:jquery-recurrence',
+  summary: 'A jQuery widget for specifying event recurrences in scheduling systems.',
+  version: '0.1.0',
+  git: 'https://github.com/aramk/jquery-recurrence',
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.0');
   api.use(['jquery', 'underscore'], 'client');
-  api.use('rrule', 'client');
+  api.use('aramk:rrule@2.1.0', 'client');
   api.add_files(['jquery.recurrence.js', 'jquery.recurrence.less'], 'client');
 });
