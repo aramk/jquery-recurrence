183225a9dc2280dac83cb3d48189aec7f8618eba
Aram Kocharyan <akarmenia@gmail.com>
2018-07-06 11:15:11 -0700

    Update version

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.6.1');
   api.use(['jquery', 'less', 'underscore'], 'client');
   api.use('aramk:rrule@2.1.0', 'client');
   api.add_files(['jquery.recurrence.js', 'jquery.recurrence.less'], 'client');
